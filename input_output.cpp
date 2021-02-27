ed0b2581cedc0a72152253bba6369af474922924
yogiparmana <49135804+yogiparmana@users.noreply.github.com>
2021-02-28 01:53:56 +0800

    Create input_output.cpp

---
+++ input_output.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Input Console" << endl;
+    cout << "-----------------" << endl;
+    
+    //input menggunakan cin >> 
+    int number; //inisialisasi variable int number
+    cout << "Masukkan Number : "; //keterangan input
+    cin >> number; // proses input
+    cout <<"output number : " << number << endl; // proses output
+    
+    
+    string kata; // inisialisasi variable string kata
+    cout << "Masukkan Kata : "; //keterangan input
+    cin >> kata; // proses input
+    cout << "output string : " << kata; // output
+   
+
+   
+   return 0;
+}
